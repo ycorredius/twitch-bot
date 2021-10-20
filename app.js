6429f7f5e23c0b31cb05fd47f597d08ba4ea2b45
shaun <ycorredius@gmail.com>
2021-10-20 17:06:08 -0400

    updating files

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
+const express = require('express')
+const app = express()
+const path = require('path')
 const tmi = require('tmi.js')
 require('dotenv').config()
+const port = process.env.PORT || "8080"
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 
 const opts = {
@@ -25,15 +32,28 @@ function onMessageHandler (target, context, msg, self) {
 
   // Remove whitespace from chat message
   const commandName = msg.trim();
-  
+  const name = target.slice(1, target.length)
   // If the command is known, let's execute it
-  if (commandName === '!dice') {
-    const num = rollDice();
-    // const userName = target.slice(1..target.length)
-    client.say(target, `You rolled a ${num}`);
-    console.log(`* Executed ${commandName} command`);
-  } else {
-    console.log(`* Unknown command ${commandName}`);
+  switch(commandName){
+    case "!dice":
+      const num = rollDice();
+      client.say(target, `You rolled a ${num}`);
+      console.log(`* Executed ${commandName} command`);
+      break;
+    case "!vote":
+      client.say(target, `@${name} voted!`)
+      break;
+    case "!insult":
+      const thisInsult = insult(name)
+      client.say(target, `${thisInsult}`)
+      console.log(`* Executed ${commandName} command`);
+      break;
+      case '!info':
+       console.log(`${context}`)
+       console.log(`${self}`)
+       break;
+    default:
+          console.log(`* Unknown command ${commandName}`);
   }
 }
 
@@ -43,11 +63,15 @@ function rollDice () {
   return Math.floor(Math.random() * sides) + 1;
 }
 
-// function insult(name){
-// 	co
-// }
+function insult(target){
+  return `@${target}...  You're mother looks like an ogre!`
+}
 
 // Called every time the bot connects to Twitch chat
 function onConnectedHandler (addr, port) {
   console.log(`* Connected to ${addr}:${port}`);
 }
+
+app.listen(port, () =>{
+  console.log(`Node is currently working on ${port}`)
+})
